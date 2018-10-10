d8df5a760fb72c024a0d55b509909f224dffa3fe
wangshengqiang <wangshengqiang@zhongandeMacBook-Pro-4.local>
2018-10-10 22:42:12 +0800

    update

---
+++ play68.js
@@ -0,0 +1,26 @@
+﻿function play68_init() {
+	updateShare(0);
+}
+function play68_submitScore(score) {
+	var str =score;
+	try{parent.__4399finishgame(str);}catch(e){}
+	//alert(score);
+	//updateShareScore(score);
+	//Play68.shareFriend();
+	// setTimeout( function() { Play68.shareFriend(); }, 500 )
+}
+function updateShare(bestScore) {
+	var descContent = "#插插插#刺激，在夹缝中生存！";
+   if(bestScore > 0) {
+		shareTitle = "我插了"+bestScore+"关，听说全世界只有4%的人玩得过第7关！";
+	}
+	else{
+		shareTitle = "听说全世界只有4%的人玩得过第7关...有点难！";
+	}
+	appid = '';
+	Play68.setShareInfo(shareTitle,descContent);
+	document.title = shareTitle;
+}
+function updateShareScore(bestScore) {
+	updateShare(bestScore);
+}
